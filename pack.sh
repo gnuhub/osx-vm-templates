3403ee5ea5320f50c41c4f5c1fe7240b53fce0a5
gnuhub <gnuhub@gmail.com>
2014-11-03 10:22:38 +0800

    fix FUSION_APP_PATH

--- pack.sh
+++ pack.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+#https://github.com/mitchellh/packer/issues/1552
+export FUSION_APP_PATH="${HOME}/Applications/VMware Fusion.app"
 cd packer
 packer build \
+  -only=vmware-iso \
   -var iso_checksum=7ebc054e4c51fe2df2f486f826ecf005 \
   -var iso_url=../out/OSX_InstallESD_10.9.4_13E28.dmg \
   template.json
