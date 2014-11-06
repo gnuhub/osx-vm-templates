981832518df145d3a7e138488eed9063367bbcfb
gnuhub <gnuhub@gmail.com>
2014-11-06 12:23:59 +0800

    fix logs location

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 chmod +x  prepare_iso/prepare_iso.sh
 BUILD_TIME=$(date +%Y%m%d_%H%m%S)
-sudo prepare_iso/prepare_iso.sh "/Users/stallman/gnuhubdata/git/osx-vm-templates/Install OS X Mavericks.app" out > ${BUILD_TIME}.log 2>&1 
+sudo prepare_iso/prepare_iso.sh "/Users/stallman/gnuhubdata/git/osx-vm-templates/Install OS X Mavericks.app" out > logs/${BUILD_TIME}.log 2>&1 
