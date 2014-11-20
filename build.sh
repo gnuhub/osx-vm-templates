99049c837e86e8f76fd1ad3e9986e58c49419775
gnuhub <gnuhub@gmail.com>
2014-11-20 18:49:07 +0800

    去掉日志输出

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
+# 快盘 files mac10.9.pkg
 chmod +x  prepare_iso/prepare_iso.sh
-BUILD_TIME=$(date +%Y%m%d_%H%m%S)
-sudo prepare_iso/prepare_iso.sh "/Users/stallman/gnuhubdata/git/osx-vm-templates/Install OS X Mavericks.app" out > logs/${BUILD_TIME}.log 2>&1 
+sudo prepare_iso/prepare_iso.sh "/Users/stallman/gnuhubdata/git/osx-vm-templates/Install OS X Mavericks.app" out
